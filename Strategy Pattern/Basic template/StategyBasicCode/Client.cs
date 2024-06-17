7a28ffdf2ef044ee1c7fa8b4e6d264e2f25c979a
Wvrmhat <86238625+Wvrmhat@users.noreply.github.com>
2024-06-17 21:18:13 +0200

    Added strategybasic code

---
+++ Strategy Pattern/Basic template/StategyBasicCode/Client.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StategyBasicCode
+{
+    class Client
+    {
+        public StrategyBase Strategy;
+        public Client(StrategyBase Strategy)
+        {
+            this.Strategy = Strategy;
+        }
+        public void CallAlgorithm()
+        {
+            Console.WriteLine(Strategy.Algorithm());
+        }
+    }
+
+}
