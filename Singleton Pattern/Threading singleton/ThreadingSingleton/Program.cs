351813e330e5efcb48caa483cbed3edafc1da42d
Wvrmhat <86238625+Wvrmhat@users.noreply.github.com>
2024-06-17 20:38:13 +0200

    Added threading singleton

---
+++ Singleton Pattern/Threading singleton/ThreadingSingleton/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ThreadingSingleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread tempThread1 = new Thread(ManageSingleton);
+            tempThread1.Name = "First thread"; //not necessary
+            tempThread1.Start();
+
+            Thread tempThread2 = new Thread(ManageSingleton);
+            tempThread2.Name = "Second thread";
+            tempThread2.Start();
+
+            Thread.CurrentThread.Name = "Main thread:";     //existing main thread that the main program is running
+            Singleton ss1 = Singleton.GetSingelton();
+            ss1.SomeMethod();                   //we somemethod
+                                                // all processes share sameshare same single singleton instance
+            tempThread1.Join();                 
+            tempThread2.Join();                 // main program cannot exit until both these two threads finish their execution
+
+            Console.ReadLine();
+        }
+
+        public static void ManageSingleton()
+        {
+            Singleton ss1 = Singleton.GetSingelton();
+            ss1.SomeMethod();
+        }
+    }
+}
---
+++ Singleton Pattern/Threading singleton/ThreadingSingleton/Singleton.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadingSingleton
+{
+    public sealed class Singleton
+    {
+        private volatile static Singleton _instance;
+        private static readonly object _lockThis = new Object();
+        private int TimesCalled = 0;
+        private Singleton() { }
+
+        public void SomeMethod()
+        {
+            //Console.WriteLine("Doing something");
+            //Not necessary to know, but could be useful
+            TimesCalled++;
+            Console.WriteLine("{0}: Doing something ({1})", System.Threading.Thread.CurrentThread.Name, TimesCalled);
+
+        }
+
+        public static Singleton GetSingelton()
+        {
+            if(_instance == null)               // double check locking slows down thread slightly so that it can hit lock and avoid object overriding
+            {
+               lock (_lockThis)                 // checks if object is set to null, it then creates the object of type singleton then jumps out the lock
+                {
+                    if(_instance == null)       // second object/thread hits null check
+                    _instance = new Singleton();
+                }
+            }
+            return _instance;
+
+        }
+
+    }
+}
