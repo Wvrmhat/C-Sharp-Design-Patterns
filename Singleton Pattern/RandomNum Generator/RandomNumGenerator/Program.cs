bb5167e0146a41e6e96f6d307c6aefe06709e44b
Wvrmhat <86238625+Wvrmhat@users.noreply.github.com>
2024-06-17 21:07:28 +0200

    Added randomnum gen

---
+++ Singleton Pattern/RandomNum Generator/RandomNumGenerator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomNumGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RandomGenerator first = RandomGenerator.GetGenerator();
+            Console.WriteLine(first.Generate(1, 5000));
+
+            RandomGenerator second = RandomGenerator.GetGenerator();
+
+            for(int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine(second.Generate(1, 1000));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Singleton Pattern/RandomNum Generator/RandomNumGenerator/RandomGenerator.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomNumGenerator
+{
+    sealed class RandomGenerator
+    {
+        private static volatile RandomGenerator generator;
+        private static volatile object _lock = new Object();
+
+        private int? Seed;
+
+        private RandomGenerator()
+        {
+            this.Seed = ExtractSeed(DateTime.Now.Ticks.ToString());
+        }
+
+        public static RandomGenerator GetGenerator()            // acts as the singleton
+        {
+            if (generator == null)
+            {
+                lock (_lock)
+                {
+                    if (generator == null)
+                    {
+                        generator = new RandomGenerator();
+                    }
+                }
+            }
+            return generator;
+        }
+
+        public int Generate(int Lowest, int Highest)
+        {
+             
+            lock (_lock)
+            {
+                int squareValue = (int)Math.Pow((double)this.Seed, 2);
+                generator.Seed = ExtractSeed(squareValue.ToString());
+
+                string squareString = squareValue.ToString();
+
+                while (squareString.Length < 4)
+                    squareString = "1" + squareString.ToString();
+
+                squareValue = int.Parse(squareString);
+
+                double value = double.Parse(squareValue.ToString().Substring(squareValue.ToString().Length / 2 - 2, 4));
+                double calculated = ((value / 9999) * Highest) + Lowest;
+
+                if (calculated > Highest)
+                    calculated = Highest;
+
+                return (int)calculated;
+            }
+        }
+
+        private static int ExtractSeed(string seedString)
+        {
+            try
+            {
+                seedString = seedString.Substring(seedString.Length - 4, 4);
+
+                if (seedString[0] == '1')
+                {
+                    seedString = "1" + seedString.Substring(1);
+                }
+
+                while(seedString.Length < 4)
+                {
+                    seedString = "1" + seedString;
+                }
+                return int.Parse(seedString);
+
+            }
+            catch
+            {
+                return ExtractSeed(DateTime.Now.Ticks.ToString());
+            }
+        }
+    }
+}
