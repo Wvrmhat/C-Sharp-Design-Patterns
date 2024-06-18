a273e88a51325024c3823d30639037fa473bd559
Wvrmhat <86238625+Wvrmhat@users.noreply.github.com>
2024-06-18 21:36:49 +0200

    Added factory method template

---
+++ Factory Method/Base code/Factory method template/FactoryMethodTemplate/ConcreteFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodTemplate
+{
+    class ConcreteFactoryA:FactoryBase
+    {
+        public override ProductBase FactoryMethod()
+        {
+            return new ConcreteProductA();
+        }
+    }
+
+    class ConcreteFactoryB : FactoryBase
+    {
+        public override ProductBase FactoryMethod()
+        {
+            return new ConcreteProductB();
+        }
+    }
+}
---
+++ Factory Method/Base code/Factory method template/FactoryMethodTemplate/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodTemplate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FactoryBase theFactory = new ConcreteFactoryA();
+            ProductBase theProduct = theFactory.FactoryMethod();
+            Console.WriteLine("Created " + theProduct.GetType().Name);
+            theFactory = new ConcreteFactoryB();
+            theProduct = theFactory.FactoryMethod();
+            Console.WriteLine("Created " + theProduct.GetType().Name);
+            Console.ReadLine();
+
+        }
+    }
+}
